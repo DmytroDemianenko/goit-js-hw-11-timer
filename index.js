9810c86e3c377d7292897c6e1dd054278120caba
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-11-08 22:36:02 +0100

    to check

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
-  });
+const dateSelect = document.querySelector("#start-date");
+dateSelect.addEventListener('input', startTimer);
+function startTimer(){
+  const selectTime = dateSelect.value; 
+  
+  const exemple = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date(selectTime),
+});
+console.log(exemple);
+}
 
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-
-const refs = {
-  dateSelect: document.querySelector("#start-date"),
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs"]')
-};
+class CountdownTimer{
+  constructor({selector, targetDate}){
+    this.selector = selector;
+    this.targetDate =  targetDate;
+    this.start();
+  };
+  start(){
+      setInterval(()=>{
+      const targetDate = this.targetDate.getTime();
+      const currentTime = new Date().getTime();
+      const timeDifference = targetDate - currentTime;
+      const timeComponent = this.getTimeComponent(timeDifference);
+      this.updateClock(timeComponent);
+    }, 1000)
+  }
+  getTimeComponent(time){
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    return {days, hours, mins, secs};
+  }
+  updateClock({days, hours, mins, secs}){
+    const day = document.querySelector(`${this.selector} [data-value="days"]`);
+    const hour = document.querySelector(`${this.selector} [data-value="hours"]`);
+    const min = document.querySelector(`${this.selector} [data-value="mins"]`);
+    const sec = document.querySelector(`${this.selector} [data-value="secs"]`);
+    day.textContent = days;
+    hour.textContent = hours;
+    min.textContent = mins;
+    sec.textContent = secs;
+    }
+}
